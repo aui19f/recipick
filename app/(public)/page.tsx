a012e29b4d9fac6b3df24f13b3a1ed8db842ff25
aui19f <aui19f@gmail.com>
2025-08-08 20:21:27 +0900

    레시피 레이아웃(스타일) 기초잡기

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,9 +2,6 @@ import Feed from "@/components/Feed";
 import db from "@/lib/db";
 
 export default async function Home() {
-  const testUser = await db.users.count();
-
-  console.log("testuser: ", testUser);
   return (
     <section className="h-full overflow-auto flex">
       <ul className="p-4 flex flex-col gap-4 m-auto sm:w-[470px]">
--- app/(public)/recipe/page.tsx
+++ app/(public)/recipe/page.tsx
@@ -1,3 +1,232 @@
+import Image from "next/image";
+
 export default function Recipe() {
-  return <>Recipe</>;
+  return (
+    <div className="h-full overflow-auto flex flex-col p-2">
+      <header></header>
+      <section className="bg-gray-100 p-2">
+        <ul className="grid  gap-8 max-w-5xl mx-auto grid-cols-1 scroll-auto sm:grid-cols-3 sm:gap-6">
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+          <li className="flex flex-col gap-1">
+            <div className="relative ">
+              <div className="aspect-square bg-gray-200"></div>
+              <div className="absolute top-1 right-1 flex items-center gap-1 py-1 px-2 bg-gray-600 rounded-lg">
+                <Image
+                  src="/icons/button_like_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+
+                <Image
+                  src="/icons/button_bookmark_active.png"
+                  width={16}
+                  height={16}
+                  alt="좋아요"
+                />
+                <span className="text-sm">1</span>
+              </div>
+            </div>
+
+            <div className="flex items-end">
+              <h4 className="flex-1 font-bold">(제목)홍차맛스콘</h4>
+              <div className="text-sm">YY/MM/DD</div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
