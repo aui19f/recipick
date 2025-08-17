581f56cb663dd49075fdfaad6a8487cd2f197a59
aui19f <aui19f@gmail.com>
2025-08-17 20:39:16 +0900

    [ci] 배포 테스트3

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -11,18 +11,18 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const { loadingStart, loadingEnd } = useLoadingStore();
-  const [skip, setSkip] = useState(0);
+  // const [skip, setSkip] = useState(0);
   const take = 20;
 
   //isFetching, refetch
-  const query = useQuery<FeedType[]>({
-    queryKey: ["feeds", skip],
-    queryFn: async () => await getFeedList(skip, take),
+  const query = useQuery({
+    queryKey: ["feeds", 0],
+    queryFn: async () => await getFeedList(0, take),
   });
 
-  const handleLoadMore = () => {
-    setSkip((prev) => prev + take);
-  };
+  // const handleLoadMore = () => {
+  //   setSkip((prev) => prev + take);
+  // };
 
   useEffect(() => {
     if (query.isFetching) loadingStart();
@@ -31,7 +31,9 @@ export default function Home() {
 
   return (
     <section className="h-full overflow-auto flex w-full">
-      <ul className="p-4 flex flex-col gap-4  w-full mx-auto sm:w-[470px]">
+      {query.error && <p>{query.error.toString()}</p>}
+
+      {/* <ul className="p-4 flex flex-col gap-4  w-full mx-auto sm:w-[470px]">
         {query.data?.map((item) => (
           <Feed key={item.id} {...item} />
         ))}
@@ -39,7 +41,7 @@ export default function Home() {
 
       {query.data?.length && query.data?.length % 20 === 0 && (
         <Button onClick={handleLoadMore}>더보기</Button>
-      )}
+      )} */}
     </section>
   );
 }
