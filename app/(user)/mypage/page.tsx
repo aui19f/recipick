f0bd274b6030b70c206b800bd12b1649cf719826
aui19f <aui19f@gmail.com>
2025-08-17 22:27:16 +0900

    [test] 테스트를 위한 로그인 임시버튼

--- app/(user)/mypage/page.tsx
+++ app/(user)/mypage/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import Button from "@/components/forms/Button";
+import { redirect } from "next/navigation";
+
 export default function Mypage() {
-  return <>Myapge</>;
+  const loginPage = () => {
+    redirect("/login");
+  };
+  return (
+    <>
+      <p>test Login</p>
+      <Button onClick={() => loginPage()}>LOGIN</Button>
+    </>
+  );
 }
