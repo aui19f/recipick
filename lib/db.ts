e546e976ec0094b27e7b1a1a32770edee88ae32e
aui19f <aui19f@gmail.com>
2025-08-17 21:59:17 +0900

    [ci] 에러로그 해결중1

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,15 @@
 // import { PrismaClient } from "@/lib/generated/prisma";
-// // import { PrismaClient } from "@prisma/client";
-// const db = new PrismaClient();
-
-// export default db;
-
-// lib/prisma.ts
-import { PrismaClient } from "@/lib/generated/prisma";
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
 
 export default db;
+
+// lib/prisma.ts
+// import { PrismaClient } from "@/lib/generated/prisma";
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const db = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+// export default db;
